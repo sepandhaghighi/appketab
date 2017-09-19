ff949a84ca8bb092554e87d0ac7f0bb78ead4012
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-09-19 13:11:11 +0430

    fix : body margin set to zero

--- javascript/assets/application.js
+++ javascript/assets/application.js
@@ -5,6 +5,7 @@ element.style.background="url(images/background/"+random_number.toString()+".jpg
 element.style.backgroundSize="cover";
 element.style.width="100%";
 element.style.height="100%";
+element.style.margin="0";
 element.style.backgroundAttachment="fixed";
 
 function resizeBackground() {
